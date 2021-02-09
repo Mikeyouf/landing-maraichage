61a2fae022943485e0095867114a4ca351e1f8e5
Mikeyouf <mik_anarchy@hotmail.com>
2021-02-09 12:14:45 +0100

    modif theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,8 +86,8 @@ module.exports = {
                 name: `gatsby-starter-default`,
                 short_name: `starter`,
                 start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
+                background_color: `#F1B631`,
+                theme_color: `#F1B631`,
                 display: `minimal-ui`,
                 icon: `src/images/logo.png`, // This path is relative to the root of the site.
             },
